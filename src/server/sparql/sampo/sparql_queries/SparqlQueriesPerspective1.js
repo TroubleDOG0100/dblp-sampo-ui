b92965c60d76d2afcbec618f802215b7307d72ea
TroubleDOG0100 <bingogrislis@gmail.com>
2025-05-19 20:06:48 +0300

    Added more fields for publications and creator perspective.

--- src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective1.js
+++ src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective1.js
@@ -7,10 +7,31 @@ export const workProperties = `
     BIND(?id AS ?uri__id)
     BIND(?id AS ?uri__dataProviderUrl)
     BIND(?id AS ?uri__prefLabel)
+
     ?id dblp:yearOfPublication ?publicationYear .
+
     ?id a ?publicationType__id .
-    ?publicationType__id rdfs:label ?publicationType__prefLabel
+    ?publicationType__id rdfs:label ?publicationType__prefLabel .
     FILTER(?publicationType__id != dblp:Publication)
+
+    ?id dblp:doi ?doi__id .
+    BIND(STRAFTER(STR(?doi__id), "https://doi.org/") as ?doi__prefLabel)
+    BIND(?doi__id as ?doi__dataProviderUrl)
+
+    ?id dblp:publishedIn ?publicationVenueStr .
+
+
+    ## TODO: Could encode whether the role is author or editor.
+    ?id dblp:createdBy ?creators__id .
+    BIND(CONCAT("/creators/page/", ENCODE_FOR_URI(STR(?creators__id))) as ?creators__dataProviderUrl)
+    ?creators__id dblp:creatorName ?creators__prefLabel .
+
+    OPTIONAL {
+      ?id dblp:publishedInStream ?publicationVenue__id .
+      ?publicationVenue__id dblp:streamTitle ?publicationVenueStr2 .
+      BIND(?publicationVenue__id as ?publicationVenue__dataProviderUrl)
+    }
+    BIND(COALESCE(?publicationVenueStr2, ?publicationVenueStr) as ?publicationVenue__prefLabel)
 
     # Data provider varētu kā instance page konkrētām publikācijas tipam.
     #BIND(?publicationType__id as )
