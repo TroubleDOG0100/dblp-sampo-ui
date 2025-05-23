316cde7e8450d9517891e4bac447864f19ac08e4
TroubleDOG0100 <bingogrislis@gmail.com>
2025-05-23 13:05:11 +0300

    New fields added for creator table views

--- src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective2.js
+++ src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective2.js
@@ -8,4 +8,26 @@ export const workProperties =
     BIND(?id AS ?uri__id)
     BIND(?id AS ?uri__dataProviderUrl)
     BIND(?id AS ?uri__prefLabel)
+
+  	?id a ?type .
+    FILTER(?type != dblp:Creator)
+    ?type rdfs:label ?creatorType .
+    FILTER(LANG(?creatorType) = "en")
+
+    OPTIONAL {
+        ?id dblp:affiliation ?affiliation .
+    }
+    OPTIONAL {
+        ?id dblp:creatorNote ?creatorNote .
+    }
+    OPTIONAL {
+      ?id owl:sameAs ?otherIds__id .
+      BIND(STR(?otherIds__id) as ?otherIds__prefLabel)
+      BIND(?otherIds__id as ?otherIds__dataProviderUrl)
+    }
+    OPTIONAL {
+        ?id dblp:orcid ?orcid__id .
+        BIND(STR(?orcid__id) AS ?orcid__prefLabel)
+        BIND(?orcid__id AS ?orcid__dataProviderUrl)
+    }
 `
